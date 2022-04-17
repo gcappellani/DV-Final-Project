e239da17033b90d0b45e8376223b894ea62289ba
Giulio Cappellani <gcappellani@dons.usfca.edu>
2022-04-17 13:26:30 -0700

    Commit #10 - Beta release

--- js/boxplot.js
+++ js/boxplot.js
@@ -1,138 +1,146 @@
 var canvasWidth = 1000;
 var canvasHeight = 500;
-var margin = { top: 70, right: 40, bottom: 55, left: 180 };
-
-var svg = d3.select("body").append("svg")
-    .attr("width",  canvasWidth)
-    .attr("height", canvasHeight)
+var margin = { top: 50, right: 40, bottom: 80, left: 180 };
 
 var width = canvasWidth - margin.left - margin.right;
 var height = canvasHeight - margin.top - margin.bottom;
 
-svg.append("text")
-    .attr("transform", "translate(100, 0)")
-    .attr("x", width/3)
-    .attr("y", 30)
-    .attr("font-family", "sans-serif")
-    .attr("font-size", "30px")
-    .text("Eminem style features overtime")
-
 var xScale = d3.scaleBand().range([0, width]);
 var yScale = d3.scaleLinear().range([height, 0]);
 
-var container_g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+function drawGraph(yValue) {
+    d3.csv("https://raw.githubusercontent.com/gcappellani/DV-Final-Project/main/data/eminem.csv",
+        function (d) {
+            year = +d['album_released'].split("/")[2]
+            if (year < 90) {
+                year += 2000
+            } else {
+                year += 1900
+            }
+            return {
+                danceability: +d['danceability'],
+                energy: +d['energy'],
+                loudness: +d['loudness'] + 25.5,
+                speechiness: +d['speechiness'],
+                acousticness: +d['acousticness'],
+                positiveness: +d['valence'],
+                tempo: +d['tempo'],
+                year: year
+            }
+        }, function (data) {
 
-d3.csv("https://raw.githubusercontent.com/gcappellani/DV-Final-Project/main/data/eminem.csv",
-    function(d) {
-        year = +d['album_released'].split("/")[2]
-        if (year < 90) { year += 2000}
-        else {year += 1900}
-        return {
-            danceability: +d['danceability'],
-            energy: +d['energy'],
-            loudness: +d['loudness'],
-            speechiness: +d['speechiness'],
-            acousticness: +d['acousticness'],
-            positiveness: +d['valence'],
-            tempo: +d['tempo'],
-            year: year
-        }
-    }, function(data) {
+            d3.select("svg").remove()
 
-    var boxesPerYear = d3.nest()
-        .key(d => d.year)
-        .rollup(function(d) {
-            quartile1 = d3.quantile(d.map(v => v.energy).sort(d3.ascending), .25)
-            median = d3.quantile(d.map(v => v.energy).sort(d3.ascending), .5)
-            quartile3 = d3.quantile(d.map(v => v.energy).sort(d3.ascending), .75)
-            min = d3.min(d.map(v => v.energy))
-            max = d3.max(d.map(v => v.energy))
-            return({quartile1: quartile1, median: median, quartile3: quartile3, min: min, max: max})
-        }).entries(data);
+            var svg = d3.select("body").append("svg")
+                .attr("width",  canvasWidth)
+                .attr("height", canvasHeight)
 
-    var years = [...new Set(data.map(d => d.year))]
+            svg.append("text")
+                .attr("transform", "translate(100, 0)")
+                .attr("x", width/3)
+                .attr("y", 30)
+                .attr("font-family", "sans-serif")
+                .attr("font-size", "30px")
+                .text("Eminem style features overtime")
 
-    xScale.domain(years);
-    yScale.domain([0, d3.max(data, function(d) { return d.energy })])
+            var container_g = svg.append("g")
+                .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
-    // Display the X-axis
-    container_g.append("g")
-        .attr("transform", "translate(0, " + height + ")")
-        .call(d3.axisBottom(xScale))
-        .append("text")
-        .attr("y", 50)
-        .attr("x", width/2)
-        .attr("stroke", "black")
-        .attr("fill", "black")
-        .attr("font-size", "15px")
-        .text("Year");
+            var boxesPerYear = d3.nest()
+                .key(d => d.year)
+                .rollup(function (d) {
+                    quartile1 = d3.quantile(d.map(v => v[yValue]).sort(d3.ascending), .25)
+                    median = d3.quantile(d.map(v => v[yValue]).sort(d3.ascending), .5)
+                    quartile3 = d3.quantile(d.map(v => v[yValue]).sort(d3.ascending), .75)
+                    min = d3.min(d.map(v => v[yValue]))
+                    max = d3.max(d.map(v => v[yValue]))
+                    return ({quartile1: quartile1, median: median, quartile3: quartile3, min: min, max: max})
+                }).entries(data);
 
-    // Display the Y-axis
-    container_g.append("g")
-        .call(d3.axisLeft(yScale))
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", -50)
-        .attr("x", -100)
-        .attr("stroke", "black")
-        .attr("fill", "black")
-        .attr("font-size", "15px")
-        .text("Energy");
+            var years = [...new Set(data.map(d => d.year))]
 
-    // Show the main vertical line
-    container_g.selectAll("vertLines")
-        .data(boxesPerYear)
-        .enter()
-        .append("line")
-        .attr("x1", d => xScale(d.key) + xScale.bandwidth()/2)
-        .attr("x2", d => xScale(d.key) + xScale.bandwidth()/2)
-        .attr("y1", d => yScale(d.value.min))
-        .attr("y2", d => yScale(d.value.max))
-        .attr("stroke", "black")
-        .style("width", 40);
+            xScale.domain(years);
+            yScale.domain([0, d3.max(data, function (d) {
+                return d[yValue]
+            })])
 
-    container_g.selectAll("minDots")
-        .data(boxesPerYear)
-        .enter()
-        .append("circle")
-        .attr("cx", d => xScale(d.key) + xScale.bandwidth()/2)
-        .attr("cy", d => yScale(d.value.min))
-        .attr("r", 2)
-        .attr("stroke", "black")
-        .style("fill", "rgb(0,0,0)")
+            // Display the X-axis
+            container_g.append("g")
+                .attr("transform", "translate(0, " + height + ")")
+                .call(d3.axisBottom(xScale))
+                .append("text")
+                .attr("y", 50)
+                .attr("x", width / 2)
+                .attr("stroke", "black")
+                .attr("fill", "black")
+                .attr("font-size", "15px")
+                .text("Year");
 
-    container_g.selectAll("maxDots")
-        .data(boxesPerYear)
-        .enter()
-        .append("circle")
-        .attr("cx", d => xScale(d.key) + xScale.bandwidth()/2)
-        .attr("cy", d => yScale(d.value.max))
-        .attr("r", 2)
-        .attr("stroke", "black")
-        .style("fill", "rgb(0,0,0)")
+            // Display the Y-axis
+            container_g.append("g")
+                .call(d3.axisLeft(yScale))
 
-    var boxWidth = 20;
-    container_g.selectAll("boxes")
-        .data(boxesPerYear)
-        .enter()
-        .append("rect")
-        .attr("x", d => xScale(d.key) - boxWidth/2 + xScale.bandwidth()/2)
-        .attr("y", d => yScale(d.value.quartile3))
-        .attr("height", d => yScale(d.value.quartile1) - yScale(d.value.quartile3))
-        .attr("width", boxWidth )
-        .attr("stroke", "black")
-        .style("fill", "rgb(184,72,72)")
+            // Show the main vertical line
+            container_g.selectAll("vertLines")
+                .data(boxesPerYear)
+                .enter()
+                .append("line")
+                .attr("x1", d => xScale(d.key) + xScale.bandwidth() / 2)
+                .attr("x2", d => xScale(d.key) + xScale.bandwidth() / 2)
+                .attr("y1", d => yScale(d.value.min))
+                .attr("y2", d => yScale(d.value.max))
+                .attr("stroke", "black")
+                .style("width", 40);
 
-    container_g.selectAll("medianLines")
-        .data(boxesPerYear)
-        .enter()
-        .append("line")
-        .attr("x1", d => xScale(d.key) - boxWidth/2 + xScale.bandwidth()/2)
-        .attr("x2", d => xScale(d.key) + boxWidth/2 + xScale.bandwidth()/2)
-        .attr("y1", d => yScale(d.value.median))
-        .attr("y2", d => yScale(d.value.median))
-        .attr("stroke", "black")
-        .style("width", 80)
+            container_g.selectAll("minDots")
+                .data(boxesPerYear)
+                .enter()
+                .append("circle")
+                .attr("cx", d => xScale(d.key) + xScale.bandwidth() / 2)
+                .attr("cy", d => yScale(d.value.min))
+                .attr("r", 2)
+                .attr("stroke", "black")
+                .style("fill", "rgb(0,0,0)")
 
-})
+            container_g.selectAll("maxDots")
+                .data(boxesPerYear)
+                .enter()
+                .append("circle")
+                .attr("cx", d => xScale(d.key) + xScale.bandwidth() / 2)
+                .attr("cy", d => yScale(d.value.max))
+                .attr("r", 2)
+                .attr("stroke", "black")
+                .style("fill", "rgb(0,0,0)")
+
+            var boxWidth = 20;
+            container_g.selectAll("boxes")
+                .data(boxesPerYear)
+                .enter()
+                .append("rect")
+                .attr("x", d => xScale(d.key) - boxWidth / 2 + xScale.bandwidth() / 2)
+                .attr("y", d => yScale(d.value.quartile3))
+                .attr("height", d => yScale(d.value.quartile1) - yScale(d.value.quartile3))
+                .attr("width", boxWidth)
+                .attr("stroke", "black")
+                .style("fill", "rgb(184,72,72)")
+
+            container_g.selectAll("medianLines")
+                .data(boxesPerYear)
+                .enter()
+                .append("line")
+                .attr("x1", d => xScale(d.key) - boxWidth / 2 + xScale.bandwidth() / 2)
+                .attr("x2", d => xScale(d.key) + boxWidth / 2 + xScale.bandwidth() / 2)
+                .attr("y1", d => yScale(d.value.median))
+                .attr("y2", d => yScale(d.value.median))
+                .attr("stroke", "black")
+                .style("width", 80)
+
+        })
+}
+
+function setGraph() {
+    yValue = d3.select("#y-value").property("value")
+    drawGraph(yValue)
+}
+
+setGraph()
--- js/bubblechart.js
+++ js/bubblechart.js
@@ -2,22 +2,10 @@ var canvasWidth = 1000;
 var canvasHeight = 650;
 var margin = { top: 100, right: 200, bottom: 100, left: 180 };
 
-var svg = d3.select("body").append("svg")
-    .attr("width",  canvasWidth)
-    .attr("height", canvasHeight)
-
 var width = canvasWidth - margin.left - margin.right;
 var height = canvasHeight - margin.top - margin.bottom;
 var minBubbleSize = 1;
 var maxBubbleSize = 10;
-
-svg.append("text")
-    .attr("transform", "translate(100, 0)")
-    .attr("x", width/2 + 40)
-    .attr("y", 30)
-    .attr("font-family", "sans-serif")
-    .attr("font-size", "30px")
-    .text("Eminem style")
 
 var xScale = d3.scaleLinear().range([0, width]);
 var yScale = d3.scaleLinear().range([height, 0]);
@@ -25,105 +13,102 @@ var zScale = d3.scaleLinear().range([minBubbleSize, maxBubbleSize]);
 var colors = colorbrewer["Reds"][8]
 var colorScale = d3.scaleLinear().range([colors[colors.length-1], colors[0]])
 
-var container_g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+function drawGraph(xValue, yValue, zValue, colorValue) {
+    d3.csv("https://raw.githubusercontent.com/gcappellani/DV-Final-Project/main/data/eminem.csv",
+        function (d) {
+            return {
+                danceability: +d['danceability'],
+                energy: +d['energy'],
+                loudness: +d['loudness'] + 25.5,
+                speechiness: +d['speechiness'],
+                acousticness: +d['acousticness'],
+                positiveness: +d['valence'],
+                tempo: +d['tempo']
+            }
+        }).then(function (data) {
 
-d3.csv("https://raw.githubusercontent.com/gcappellani/DV-Final-Project/main/data/eminem.csv",
-    function(d) {
-        return {
-            danceability: +d['danceability'],
-            energy: +d['energy'],
-            loudness: +d['loudness'],
-            speechiness: +d['speechiness'],
-            acousticness: +d['acousticness'],
-            positiveness: +d['valence'],
-            tempo: +d['tempo']
-        }
-    }).then( function(data) {
+        d3.select("svg").remove()
 
-    // We sort the cars by mpg descending order because when we will be displaying
-    // the different dots we want the bigger ones to be displayed first, so that
-    // they will not cover the smaller dots
-    var sortedSongs = d3.rollup(data, d => d).sort((a,b) => d3.descending(a.positiveness, b.positiveness))
+        var svg = d3.select("body").append("svg")
+            .attr("width",  canvasWidth)
+            .attr("height", canvasHeight)
 
-    xScale.domain([0, d3.max(data, d => d.danceability)])
-    yScale.domain([0, d3.max(data, d => d.speechiness)])
-    zScale.domain([d3.min(data, d => d.positiveness), d3.max(data, d => d.positiveness)])
-    colorScale.domain([d3.max(data, d => d.energy), d3.min(data, d => d.energy)])
+        svg.append("text")
+            .attr("transform", "translate(100, 0)")
+            .attr("x", width/2)
+            .attr("y", 30)
+            .attr("font-size", "24px")
+            .attr("font-family", "sans-serif")
+            .text("Eminem style")
 
-    var legendLinear = d3.legendColor()
-        .shapeWidth(20)
-        .shapeHeight(30)
-        .cells(5)
-        .orient('vertical')
-        .ascending(false)
-        .scale(colorScale);
+        var container_g = svg.append("g")
+            .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
-    var legendSize = d3.legendSize()
-        .shape('circle')
-        .shapePadding(15)
-        .labelOffset(20)
-        .orient('vertical')
-        .scale(zScale)
+        // We sort the cars by mpg descending order because when we will be displaying
+        // the different dots we want the bigger ones to be displayed first, so that
+        // they will not cover the smaller dots
+        var sortedSongs = d3.rollup(data, d => d).sort((a, b) => d3.descending(a[zValue], b[zValue]))
 
-    // Display the X-axis
-    container_g.append("g")
-        .attr("transform", "translate(0, " + height + ")")
-        .call(d3.axisBottom(xScale))
-        .append("text")
-        .attr("y", 50)
-        .attr("x", width/2)
-        .attr("stroke", "black")
-        .attr("fill", "black")
-        .attr("font-size", "15px")
-        .text("Danceability")
+        xScale.domain([0, d3.max(data, d => d[xValue])])
+        yScale.domain([0, d3.max(data, d => d[yValue])])
+        zScale.domain([d3.min(data, d => d[zValue]), d3.max(data, d => d[zValue])])
+        colorScale.domain([d3.max(data, d => d[colorValue]), d3.min(data, d => d[colorValue])])
 
-    // Display the Y-axis
-    container_g.append("g")
-        .call(d3.axisLeft(yScale))
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", -50)
-        .attr("x", -100)
-        .attr("stroke", "black")
-        .attr("fill", "black")
-        .attr("font-size", "15px")
-        .text("Speechiness")
+        var legendLinear = d3.legendColor()
+            .shapeWidth(20)
+            .shapeHeight(30)
+            .cells(5)
+            .orient('vertical')
+            .ascending(false)
+            .scale(colorScale);
 
-    // Display dots
-    container_g.selectAll("circle").data(sortedSongs)
-        .enter().append("circle")
-        .attr("cy", d => yScale(d.speechiness))
-        .attr("cx", d => xScale(d.danceability))
-        .attr("r", d => zScale(d.positiveness))
-        .attr("fill", d => colorScale(d.energy))
-        .attr("stroke", "white")
-        .attr("stroke-width", .5)
+        var legendSize = d3.legendSize()
+            .shape('circle')
+            .shapePadding(15)
+            .labelOffset(20)
+            .orient('vertical')
+            .scale(zScale)
 
-    container_g.append("g")
-        .attr("class", "legendLinear")
-        .attr("transform", "translate(" + (width + 90) + "," + 10 + ")")
-    container_g.select(".legendLinear")
-        .call(legendLinear)
-    container_g.append("g")
-        .attr("transform", "translate(" + (width + 90) + "," + 0 + ")")
-        .append("text")
-        .attr("stroke", "black")
-        .attr("fill", "black")
-        .attr("font-size", "15px")
-        .text("Energy")
+        // Display the X-axis
+        container_g.append("g")
+            .attr("transform", "translate(0, " + height + ")")
+            .call(d3.axisBottom(xScale))
 
-    container_g.append("g")
-        .attr("class", "legendSize")
-        .attr("transform", "translate(" + (width + 90) + "," + 300 + ")")
-    container_g.select(".legendSize")
-        .call(legendSize)
-    container_g.append("g")
-        .attr("transform", "translate(" + (width + 90) + "," + 285 + ")")
-        .append("text")
-        .attr("stroke", "black")
-        .attr("fill", "black")
-        .attr("font-size", "15px")
-        .text("Positiveness")
+        // Display the Y-axis
+        container_g.append("g")
+            .call(d3.axisLeft(yScale))
 
-})
+        // Display dots
+        container_g.selectAll("circle").data(sortedSongs)
+            .enter().append("circle")
+            .attr("cy", d => yScale(d[yValue]))
+            .attr("cx", d => xScale(d[xValue]))
+            .attr("r", d => zScale(d[zValue]))
+            .attr("fill", d => colorScale(d[colorValue]))
+            .attr("stroke", "white")
+            .attr("stroke-width", .5)
+
+        container_g.append("g")
+            .attr("class", "legendLinear")
+            .attr("transform", "translate(" + (width + 90) + "," + 10 + ")")
+        container_g.select(".legendLinear")
+            .call(legendLinear)
+
+        container_g.append("g")
+            .attr("class", "legendSize")
+            .attr("transform", "translate(" + (width + 90) + "," + 300 + ")")
+        container_g.select(".legendSize")
+            .call(legendSize)
+
+    })
+}
+
+function setGraph() {
+    xValue = d3.select("#x-value").property("value")
+    yValue = d3.select("#y-value").property("value")
+    zValue = d3.select("#z-value").property("value")
+    colorValue = d3.select("#color-value").property("value")
+    drawGraph(xValue, yValue, zValue, colorValue);
+}
+
+setGraph()
